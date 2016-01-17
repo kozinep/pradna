bb39ba6e9fe352aaae8774cd46d4a1e0727f3ae4
kozinep <kozinep@yandex.ru>
2016-01-17 23:31:20 +0300

    изменил настройку в settings.py для sqlite бд.Нужна для тестовых целей.Боевая - mysql

--- pradna_django/myproject/myproject/settings.py
+++ pradna_django/myproject/myproject/settings.py
@@ -78,14 +78,18 @@ FILE_CHARSET = 'utf-8'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#    	'ENGINE': 'django.db.backends.mysql',
+#	'NAME': 'blogdb',
+#	'USER': 'blogdb',
+#	'PASSWORD': 'Citibank09',
+#	'HOST': '127.0.0.1',
+#	'PORT': '3306',
+#     }
     'default': {
-    	'ENGINE': 'django.db.backends.mysql',
-	'NAME': 'blogdb',
-	'USER': 'blogdb',
-	'PASSWORD': 'Citibank09',
-	'HOST': '127.0.0.1',
-	'PORT': '3306',
-     }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'sqlite.db'),
+    }
 }
 
 
