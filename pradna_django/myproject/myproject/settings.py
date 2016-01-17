1625cf202fbc3224fe6a84ea2683ae49f184ffab
kozinep <kozinep@yandex.ru>
2016-01-18 00:25:45 +0300

    добавил контекст для обслуживания статики

--- pradna_django/myproject/myproject/settings.py
+++ pradna_django/myproject/myproject/settings.py
@@ -65,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #add to serve static in dev
+                'django.core.context_processors.static'
             ],
         },
     },
