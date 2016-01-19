41e24e81375227c02a76ad496fd3723a0049a5a6
ugamon <jurybulich22@gmail.com>
2016-01-19 17:37:32 +0300

    для обслуживания изображений в админке  Changes to be committed: 	modified:   pradna_django/myproject/myproject/admin.py 	modified:   pradna_django/myproject/myproject/admin.pyc 	modified:   pradna_django/myproject/myproject/models.py 	modified:   pradna_django/myproject/myproject/models.pyc 	modified:   pradna_django/myproject/myproject/views.py 	modified:   pradna_django/myproject/myproject/views.pyc 	modified:   pradna_django/myproject/templates/home.html

--- pradna_django/myproject/myproject/models.py
+++ pradna_django/myproject/myproject/models.py
@@ -4,4 +4,7 @@ class news_Main(models.Model):
     news_date = models.DateField()
     news_text = models.CharField(max_length=50)
 
+class carousel_Main(models.Model):
+    carousel_image = models.ImageField(upload_to='static/images')
+    carousel_alt = models.TextField(max_length=20)
 
--- pradna_django/myproject/myproject/views.py
+++ pradna_django/myproject/myproject/views.py
@@ -3,14 +3,13 @@
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from myproject.models import news_Main
+from myproject.models import news_Main,carousel_Main
 
 
 def Home (request):
-
-#database queryset
-	all_entries = news_Main.objects.all()
-	return render_to_response('home.html',context={'template_all_entries': all_entries})
+    news_entries = news_Main.objects.all()
+    carousel_entries = carousel_Main.objects.all()
+    return render_to_response('home.html',context={'template_all_entries': news_entries,"carousel_entries":carousel_entries})
 
 def Carousel(request):
     return  render_to_response('mainpage_carousel.html')
